113325d5eb5e2c0f3eae9f4abb8767d29f2e4222
Cloud Chase <cloudchase@m1pro.attlocal.net>
2024-01-21 18:34:55 -0600

    testing pipeline

--- code/app.py
+++ code/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from Finbert</h2>'
+    return '<h1>Howdy from Finbert</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
